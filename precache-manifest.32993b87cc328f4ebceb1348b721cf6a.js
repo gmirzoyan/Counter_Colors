1a7a725111cc8fe6ed31f555a4e5e915ed59f413
Gayane Mirzoyan <gayusha@gmail.com>
2020-12-17 22:44:57 -0800

    Updates

---
+++ precache-manifest.32993b87cc328f4ebceb1348b721cf6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34fc9e357b2d7c8132cb09cf9f8dc89",
+    "url": "/Counter_Colors/index.html"
+  },
+  {
+    "revision": "ab9a58ab0bd0362098c7",
+    "url": "/Counter_Colors/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "ab9a58ab0bd0362098c7",
+    "url": "/Counter_Colors/static/js/2.0c443eb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter_Colors/static/js/2.0c443eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303bf381138c96f48660",
+    "url": "/Counter_Colors/static/js/main.4d1f9b64.chunk.js"
+  },
+  {
+    "revision": "b9ac6cd381c82f74a82e",
+    "url": "/Counter_Colors/static/js/runtime-main.ffa472f2.js"
+  },
+  {
+    "revision": "511e1280c7db8a735b92d1cbbc901948",
+    "url": "/Counter_Colors/static/media/sorting.511e1280.jpg"
+  }
+]);
